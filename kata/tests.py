aad936160ea18ef6a028947ce1f3b0699ca42943
jaruano <ja.ruano@uniandes.edu.co>
2019-03-24 09:19:57 -0500

    listar portafolios existentes (rojo)

--- kata/tests.py
+++ kata/tests.py
@@ -2,4 +2,7 @@ from django.test import TestCase
 
 # Create your tests here.
 class KataTestCase(TestCase):
-    
+    def testPaso1ListaPortafolios(self):
+        url='api/portafolios'
+        response =  self.client.get(url, format='json')
+        self.assertEqual(response.status_code , 200)
--- kata2/urls.py
+++ kata2/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', include('kata.urls')),
+     path('api/', include('kata.urls')),
 ]
