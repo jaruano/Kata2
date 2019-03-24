b696f6fae3950bddec4d756593192a7a9cc8a832
Rafael <raf_berm2@yahoo.com>
2019-03-24 10:06:39 -0500

    Listar portafolios número específico (ROJO)

--- kata/tests.py
+++ kata/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+import json
+from django.contrib.auth.models import User
+from .models import Portafolio
 
 # Create your tests here.
 class KataTestCase(TestCase):
@@ -6,3 +9,11 @@ class KataTestCase(TestCase):
         url='/api/portafolios'
         response =  self.client.get(url, format='json')
         self.assertEqual(response.status_code , 200)
+
+    def testPaso2ListaPortafoliosElementosEspecificos(self):
+        userModel = User.objects.create_user(username="test", password="test", first_name="test", last_name="test", email="test@test.com")
+        portafolio = Portafolio.objects.create(propietario=userModel)
+        url='/api/portafolios'
+        response =  self.client.get(url, format='json')
+        portafolios = json.loads(response.content)
+        self.assertEqual(len(portafolios), 1)
